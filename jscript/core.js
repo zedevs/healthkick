740cb35c4494af77f1e594b46208f129dd608b27
James Addison <J4mes@live.co.uk>
2013-03-27 19:23:07 +0000

    updates

--- jscript/core.js
+++ jscript/core.js
@@ -1,22 +1,37 @@
+/* ONLY ALLOW SCROLLING ON THE CONTENT AREA */
 $(document).on('touchmove', function(e) {
     if (!$(e.target).parents('#content').length) {
         e.preventDefault();
     }
 });
-// Load Cordova.
+
+/* READY LISTENER */
 document.addEventListener("deviceready", onDeviceReady, false);
 
-// Run Ready Scripts.
+/* RUN SCRIPTS WHEN READY */
 function onDeviceReady() {
 	var db = window.openDatabase("Database", "1.0", "Healthkick", 200000);
 	db.transaction(installDB);
 }
 
 
-// Install initial database.
+/* INSTALL THE DATABASE ON THE FIRST RUN */
 function installDB(trans) {
-	trans.executeSql('CREATE TABLE IF NOT EXISTS settings (ID VARCHAR(255) PRIMARY KEY, value TEXT)');
-	//trans.executeSql('INSERT INTO settings (id, data) VALUES ("NAME", NULL)');
-	//trans.executeSql('INSERT INTO settings (id, data) VALUES ("AGE", NULL)');
-	//trans.executeSql('INSERT INTO settings (id, data) VALUES ("GENDER", NULL)');
+	var firstRun = window.localStorage.getItem("firstRun");
+	if(firstRun == null){
+		window.localStorage.setItem("firstRun", "false"); 
+		trans.executeSql('CREATE TABLE IF NOT EXISTS settings (ID VARCHAR(255) PRIMARY KEY, string TEXT, integer INT(255))');
+		trans.executeSql('INSERT INTO settings (id, string, integer) VALUES ("NAME", NULL, NULL)');
+		trans.executeSql('INSERT INTO settings (id, string, integer) VALUES ("AGE", NULL, NULL)');
+		trans.executeSql('INSERT INTO settings (id, string, integer) VALUES ("GENDER", NULL, NULL)');
+	}
+}
+
+/* CHECK IF THE APP HAS CONNECTIVITY */
+function checkConnection() {
+	var checkConnection = navigator.connection.type;
+	if(checkConnection !== Connection.UNKOWN && checkConnectiom !== Connection.NONE){
+		return true;
+	}
+	return false
 }
