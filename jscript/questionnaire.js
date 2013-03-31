56cf992f9496738ef2bb035fe4d370bfa65048c6
James Addison <J4mes@live.co.uk>
2013-03-31 21:27:07 +0100

    Setup a server with a MySQL server, which feeds random JSON questions for questionnaire. Processes and validates the questionaire. UI Fixes.

---
+++ jscript/questionnaire.js
@@ -0,0 +1,79 @@
+/* CREATE QUESTIONNAIRE HANDLER */
+$(document).ready(function(){
+	var questions_loaded = false;
+	
+	document.addEventListener("deviceready", onReadySocialZone, false);
+	
+	/* RUN SCRIPTS WHEN READY */
+	function onReadySocialZone() {
+		if(checkConnection() == false){
+			$('.modal-internet-error, .dim').show();
+		}
+	    
+		$('.retry-internet-connection').click(function(){
+			if(checkConnection() == true){
+				$('.modal-internet-error, .dim').hide();
+				loadQuestions();
+				$('.modal-internet-error, .dim').hide(); $('.modal-downloading, .dim').show();
+			}
+		});
+	}
+	
+	/* LISTEN FOR CONNECTION DROP */
+	document.addEventListener("offline", function() { 
+		if(questions_loaded == false){
+			$('.modal-box').hide(); $('.modal-internet-error, .dim').show();
+		}		
+	}, false);
+	
+	/* LISTEN FOR RE-CONNECTION */
+	document.addEventListener("online", function() { 
+	if(questions_loaded == false){
+		loadQuestions();
+		$('.modal-internet-error, .dim').hide(); $('.modal-downloading, .dim').show();
+	}
+	}, false);
+	
+	function loadQuestions(){
+		var storeAnswers = new Array();
+		$.getJSON('http://jqmes.com/healthkick/daily_questions.php', function(data) {
+		questions_loaded = true;
+		$('.modal-downloading, .dim').hide();
+		  $.each(data, function(questionKey, questionData) {
+		  	var question = '<p class="questionnaire_question">'+questionData['question']+'</p>';
+		  	storeAnswers[(questionKey+1)] = questionData['correct_answer'];
+		  	 $.each(questionData['answers'], function(answerKey, answerData) {
+		  	 	question += '<fieldset class="questionnaire_answer">';
+		  	 	question += '<label for="question_'+(questionKey+1)+'_answer_'+(answerKey+1)+'">'+(answerKey+1)+') '+answerData+'</label>';
+		  	 	question += '<input type="radio" id="question_'+(questionKey+1)+'_answer_'+(answerKey+1)+'" name="question_'+(questionKey+1)+'" value="'+(answerKey+1)+'">';
+		  	 	question += '</fieldset>';
+		  	 	
+		  	 });
+		  	$('#questionnaire_container .actions').before(question);
+		  });
+		  $('#questionnaire_container').show();
+		});
+	}
+	loadQuestions();
+	
+	$('#questionnaire_container').submit(function(){
+		if(storeAnswers.length > 0){
+			var correct = 0;
+			var wrong = 0;
+			for(var i = 1; i < storeAnswers.length; i++){
+				if($('input[name="question_'+i+'"]:checked').val() == storeAnswers[i]){
+					correct++;
+				}else{
+					wrong++;
+				}
+			}
+			var percentage = Math.round(correct/(correct+wrong) * 100);
+			$('#questionnaire_percentage').text(percentage);
+			$('.modal-questionnaire-results, .dim').show();
+				
+		}
+		
+		return false;
+	});
+});
+
--- jscript/social_zone.js
+++ jscript/social_zone.js
@@ -1,23 +1,27 @@
 /* CREATE POST HANDLER */
 $(document).ready(function(){
+	document.addEventListener("deviceready", onReadySocialZone, false);
+	
+	/* RUN SCRIPTS WHEN READY */
+	function onReadySocialZone() {
+		if(checkConnection() == false){
+			$('.modal-internet-error, .dim').show();
+		}
+	    
+		$('.retry-internet-connection').click(function(){
+			if(checkConnection() == true){
+				$('.modal-internet-error, .dim').hide();
+			}
+		});
+	}
+	
+	/* LISTEN FOR CONNECTION DROP */
+	document.addEventListener("offline", function() { $('.modal-box').hide(); $('.modal-internet-error, .dim').show(); }, false);
+	
+	/* LISTEN FOR RE-CONNECTION */
+	document.addEventListener("online", function() { $('.modal-internet-error, .dim').hide(); }, false);
+	
 	$('.button-create-post').click(function(){
 		$('.modal-create-post, .dim').show();
 	});
 });
-document.addEventListener("deviceready", onReadySocialZone, false);
-
-/* RUN SCRIPTS WHEN READY */
-function onReadySocialZone() {
-	if(checkConnection() == false){
-		$('.modal-internet-error, .dim').show();
-	}
-    
-	$('.retry-internet-connection').click(function(){
-		if(checkConnection() == true){
-			$('.modal-internet-error, .dim').hide();
-		}
-	});
-}
-
-/* LISTEN FOR CONNECTION DROP */
-document.addEventListener("offline", function() { $('.modal-internet-error, .dim').show(); }, false);
