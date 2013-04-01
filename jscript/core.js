5dd02529f4738e337bdfb10e92948837dc386c54
James Addison <J4mes@live.co.uk>
2013-04-01 17:55:47 +0100

    Bug fixes

--- jscript/core.js
+++ jscript/core.js
@@ -1,38 +1,37 @@
 var db;
-/* ONLY ALLOW SCROLLING ON THE CONTENT AREA */
-$(document).on('touchmove', function(e) {
-    if (!$(e.target).parents('#content').length) {
-        e.preventDefault();
-    }
-});
-
-/* READY LISTENER */
-document.addEventListener("deviceready", onDeviceReady, false);
-onDeviceReady();
-
-/* RUN SCRIPTS WHEN READY */
-function onDeviceReady() {
-	var appConfigured = window.localStorage.getItem("appConfigured");
-	var current_location = window.location.toString();
-	if(appConfigured == null && current_location.substr(-17) != "first_launch.html"){
-		window.location = "first_launch.html";	
+$(document).ready(function() {
+	/* ONLY ALLOW SCROLLING ON THE CONTENT AREA */
+	$(document).on('touchmove', function(e) {
+	    if (!$(e.target).parents('#content').length) {
+	        e.preventDefault();
+	    }
+	});
+	
+	/* RUN SCRIPTS WHEN READY */
+	function onDeviceReady() {
+		var appConfigured = window.localStorage.getItem("appConfigured");
+		var current_location = window.location.toString();
+		if(appConfigured == null && current_location.substr(-17) != "first_launch.html"){
+			window.location = "first_launch.html";	
+		}
+		db = window.openDatabase("Database", "1.0", "Healthkick", 200000);
 	}
-	db = window.openDatabase("Database", "1.0", "Healthkick", 200000);
-}
-
-/* CHECK IF THE APP HAS CONNECTIVITY */
-function checkConnection() {
-    var checkConnection = navigator.connection.type;
-	if(checkConnection != Connection.UNKNOWN && checkConnection != Connection.NONE){
-        return true;
+	onDeviceReady();
+	
+	/* CHECK IF THE APP HAS CONNECTIVITY */
+	function checkConnection() {
+	    var checkConnection = navigator.connection.type;
+		if(checkConnection != Connection.UNKNOWN && checkConnection != Connection.NONE){
+	        return true;
+		}
+		return false;
 	}
-	return false;
-}
-
-/* MODAL-BOX HANDLER */
-$(document).ready(function(){
-    $('.modal-remove').click(function(){
-		$(this).parents('.modal-box').hide();
-		$('.dim').hide();
-    });
+	
+	/* MODAL-BOX HANDLER */
+	$(document).ready(function(){
+	    $('.modal-remove').click(function(){
+			$(this).parents('.modal-box').hide();
+			$('.dim').hide();
+	    });
+	});
 });
